12aafffff95b59e9b886a8c58737cfe7f1f3823f
Adam Lavin <adam.lavin@upp.ai>
2020-04-25 01:32:36 +0100

    Update message

--- pages/items.tsx
+++ pages/items.tsx
@@ -4,7 +4,9 @@ import { Container } from '@material-ui/core';
 export default function Items() {
   return (
     <Container maxWidth="sm">
-      Here's a page about items
+      Here's a page about items. It hasn't been built yet. :(
+      <br /><br />
+      Yell at LavaToaster to write more code.
     </Container>
   );
 }
